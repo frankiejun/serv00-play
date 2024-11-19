904d3dcc249df6c55fd118f8525fc8094d20458a
frankiejun <frankiejun@gmail.com>
2024-11-19 15:38:05 +0800

    fixed:wssh重启不成功问题

--- keepalive.sh
+++ keepalive.sh
@@ -175,7 +175,7 @@ startSunPanel(){
 startWebSSH(){
   cd ${installpath}/serv00-play/webssh
   ssh_port=$(jq -r ".port" config.json)
-  cmd="nohup wssh --port=$ssh_port  --fbidhttp=False >/dev/null 2>&1 &"
+  cmd="nohup ./wssh --port=$ssh_port  --fbidhttp=False >/dev/null 2>&1 &"
   eval "$cmd"
 }
 
