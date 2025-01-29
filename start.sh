c354ffd2b3555560ede21486cba1b4ecec499150
frankiejun <frankiejun@gmail.com>
2025-01-29 14:11:29 +0800

    增加版本号

--- start.sh
+++ start.sh
@@ -3005,9 +3005,17 @@ vip_statement() {
   return 0
 }
 
-getVer() {
+getLatestVer() {
   ver=$(git ls-remote --tags https://github.com/frankiejun/serv00-play.git | awk -F/ '{print $3}' | sort -V | tail -n 1)
   echo $ver
+}
+getCurrentVer() {
+  ver=$(git describe --tags --abbrev=0 2>/dev/null)
+  if [ $? -ne 0 ]; then
+    echo null
+  else
+    echo $ver
+  fi
 }
 
 showMenu() {
@@ -3016,7 +3024,7 @@ showMenu() {
   echo -e "${CYAN}${art_wrod}${RESET}"
   echo -e "${GREEN} 饭奇骏频道:https://www.youtube.com/@frankiejun8965 ${RESET}"
   echo -e "${GREEN} TG交流群:https://t.me/fanyousuiqun ${RESET}"
-  echo -e "${GREEN} 版本号:$(getVer) ${RESET}"
+  echo -e "${GREEN} 当前版本号:$(getCurrentVer) 最新版本号:$(getLatestVer) ${RESET}"
   echo "<------------------------------------------------------------------>"
   echo "请选择一个选项:"
 
